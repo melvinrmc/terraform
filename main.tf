2bc58c1d8f9db8db0e0f32467ae4a06444177ba6
melvinrmc <melvinrmc@hotmail.com>
2020-01-31 10:08:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "aws_instance" "kaeptn-eichhorn" {
   ami = "ami-04b9e92b5572fa0d1"
   instance_type = "t2.micro"
 }
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = var.my_id_rsa_dot_pub
+}
