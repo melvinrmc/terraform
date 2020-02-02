6efafebca3961a888524c64ecaf52d9d774cb8c9
melvinrmc <melvinrmc@hotmail.com>
2020-02-01 22:27:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "kaeptn-eichhorn" {
   ami = "ami-04b9e92b5572fa0d1"
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.allow_ssh.name]
 }
 
 variable "MY_ID_RSA" {
