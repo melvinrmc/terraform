9b840292c0fc49882dfc9b9b9d8ff69719573c6f
melvinrmc <melvinrmc@hotmail.com>
2020-02-13 17:14:38 -0600

    Update node.tf

--- node.tf
+++ node.tf
@@ -1,19 +1,6 @@
-provider "aws" {
-  region = "us-east-1"
-}
- 
 resource "aws_instance" "node1" {
   ami = "ami-02eac2c0129f6376b"
   instance_type = "t2.medium"
   key_name = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.allow_ssh.name]
-}
-
-variable "MY_ID_RSA" {
-  type = string  
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = var.MY_ID_RSA
 }
