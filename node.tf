f81cf824776801cfe254f83c7e26e05cda2e2036
melvinrmc <melvinrmc@hotmail.com>
2020-02-14 16:59:16 -0600

    Update node.tf

--- node.tf
+++ node.tf
@@ -2,5 +2,7 @@ resource "aws_instance" "node1" {
   ami = "ami-02eac2c0129f6376b"
   instance_type = "t2.medium"
   key_name = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.allow_ssh.name]
+  security_groups = [ aws_security_group.allow_ssh.name
+                     ,aws_security_group.allow_k8s_node.name
+                    ]
 }
